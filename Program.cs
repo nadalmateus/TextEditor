b8d70a8bb2c0ec2781cfaa7953ab5d0a50ed2d01
Mateus Nadal <nadalmateus@hotmail.com>
2021-05-01 11:04:09 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace textEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
